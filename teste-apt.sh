88c37e8edbf9bd757b5df7624ef90c93b2c22621
Nestor Junior <nestor.junior@gmail.com>
2025-08-29 00:01:16 -0300

    Update teste-apt.sh

--- teste-apt.sh
+++ teste-apt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update && apt upgrade -y
-local a_csv_file="apt_apps.csv"
+a_csv_file="apt_apps.csv"
 echo -e "${AMARELO}--- INICIANDO INSTALAÇÕES VIA APT ---${NC}"
 tail -n +2 "$a_csv_file" | while IFS=, read -r app_name installer_name description; do
     echo -e "${VERDE}Instalando ${app_name} (${description})..."
