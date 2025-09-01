8a44b7fc3c616ed5f5bc7439fb0c942038c7e2e3
netTask <nestor.junior@gmail.com>
2025-09-01 14:02:44 -0300

    ...

--- install.sh
+++ install.sh
@@ -4,15 +4,16 @@ rm meudebian.sh
 rm teste-apt.sh
 rm *.csv
 
-# testa se existe deb instalado
-wget -O curl.deb https://debian.pkgs.org/13/debian-main-amd64/curl_8.14.1-2_amd64.deb.html
-apt install ./curl.deb -y
-rm curl.deb
+#curl -L -o appimage_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/appimage_apps.csv && chmod +x appimage_apps.csv
+#curl -L -o apt_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/apt_apps.csv && chmod +x apt_apps.csv
+#curl -L -o deb_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/deb_apps.csv && chmod +x deb_apps.csv
+#curl -L -o flatpak_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/flatpak_apps.csv && chmod +x flatpak_apps.csv
+#curl -L -o meudebian.sh https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/meudebian.sh && chmod +x meudebian.sh
+#curl -L -o teste-apt.sh https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/teste-apt.sh && chmod +x teste-apt.sh
 
-curl -L -o appimage_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/appimage_apps.csv && chmod +x appimage_apps.csv
-curl -L -o apt_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/apt_apps.csv && chmod +x apt_apps.csv
-curl -L -o deb_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/deb_apps.csv && chmod +x deb_apps.csv
-curl -L -o flatpak_apps.csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/flatpak_apps.csv && chmod +x flatpak_apps.csv
-curl -L -o meudebian.sh https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/meudebian.sh && chmod +x meudebian.sh
-curl -L -o teste-apt.sh https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/teste-apt.sh && chmod +x teste-apt.sh
-
+wget https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/appimage_apps.csv
+wget https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/apt_apps.csv
+wget https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/deb_apps.csv
+wget csv https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/flatpak_apps.csv
+wget https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/meudebian.sh
+wget https://raw.githubusercontent.com/nettaskjr/meudebian/refs/heads/main/teste-apt.sh
